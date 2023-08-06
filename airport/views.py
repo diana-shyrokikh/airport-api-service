d86819e1c28891e0b6f2848bbb1baac4d171f156
Diana Shyrokikh <diashiroks@gmail.com>
2023-08-06 21:15:09 +0300

    Added filters to Crew view

--- airport/views.py
+++ airport/views.py
@@ -284,8 +284,17 @@ class CrewView(viewsets.ModelViewSet):
     def get_queryset(self):
         queryset = self.queryset
 
+        first_name = self.request.query_params.get("first_name")
+        last_name = self.request.query_params.get("last_name")
+
         if self.action != "destroy":
             queryset = Crew.objects.prefetch_related("flights",)
+
+        if first_name:
+            queryset = queryset.filter(first_name__icontains=first_name)
+
+        if last_name:
+            queryset = queryset.filter(last_name__icontains=last_name)
 
         return queryset
 
