966dc293a1dcb919fa735efd5928cb9fc7ff3058
Diana Shyrokikh <diashiroks@gmail.com>
2023-08-08 02:33:01 +0300

    Added CityModelTests

--- airport/tests/models.py
+++ airport/tests/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from airport.models import (
@@ -20,5 +21,21 @@ class CountryModelTests(TestCase):
         country = Country.objects.create(name="Ukraine")
 
         self.assertEqual(country.name, str(country))
-        
+
+
+class CityModelTests(TestCase):
+    def setUp(self) -> None:
+        self.country = Country.objects.create(name="Ukraine")
+
+    def test_city_str(self):
+        city = City.objects.create(name="Kyiv", country=self.country)
+
+        self.assertEqual(city.name, str(city))
+
+    def test_validate_name_city(self):
+        invalid_names = ["   ", "123Kyiv", ".,", "Kyiv_"]
+
+        for name in invalid_names:
+            with self.assertRaises(ValidationError):
+                City.objects.create(name=name, country=self.country)
 
