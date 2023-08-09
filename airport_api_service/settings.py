f4212d1cf27f7aa36ca40f059b17fd3cd149d0e1
Diana Shyrokikh <diashiroks@gmail.com>
2023-08-09 21:33:39 +0300

    Turned off debug mode

--- airport_api_service/settings.py
+++ airport_api_service/settings.py
@@ -28,12 +28,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+]
 
 INTERNAL_IPS = [
-    '127.0.0.1',
+    "127.0.0.1",
 ]
 
 # Application definition
