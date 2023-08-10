6a14d08263c4cd5268f3b291c4faa7d807dd72b2
Diana Shyrokikh <diashiroks@gmail.com>
2023-08-10 13:13:42 +0300

    Edited by flake8

--- airport_api_service/urls.py
+++ airport_api_service/urls.py
@@ -25,9 +25,21 @@ from drf_spectacular.views import (
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    path("api/v1/doc/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/v1/doc/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    path("api/v1/doc/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path(
+        "api/v1/doc/",
+        SpectacularAPIView.as_view(),
+        name="schema"
+    ),
+    path(
+        "api/v1/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui"
+    ),
+    path(
+        "api/v1/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc"
+    ),
 
     path("api/v1/airport/", include("airport.urls", namespace="airport")),
     path("api/v1/user/", include("user.urls", namespace="user")),
